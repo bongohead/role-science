ffaed2e77ac21aa1c47852857c1fe8bae4e1fc4f
Charles Y <charles@econforecasting.com>
2025-08-23 05:42:44 +0000

    cleanup

--- unix_install_packages.sh
+++ unix_install_packages.sh
@@ -19,6 +19,5 @@ pip install kernels==0.9.0 # As of Aug 2025, dep for GPT-OSS (supports fa3 w/att
 pip install triton==3.4.0
 pip install tiktoken
 
-
 # Analysis
 pip install --extra-index-url=https://pypi.nvidia.com "cudf-cu12==25.8.*" "cuml-cu12==25.8.*"
